edaddda6d75d219ced6d4d7db086a16cf7395883
Nguyen Dao Duy Kien <kienlop9altt2014@gmail.com>
2022-01-09 11:30:24 +0700

    update comment

--- src/components/layouts/home-feed/posts/Comments/CommentPost.tsx
+++ src/components/layouts/home-feed/posts/Comments/CommentPost.tsx
@@ -1,13 +1,31 @@
 import { Formik } from "formik";
 import { FormItem, Input } from "formik-antd";
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import AvatarBase from "src/components/base/avatar/Avatar";
 import { userSelector } from "src/ducks/user/selector";
 import { CommemtWrapper } from "./style";
+import uploadIcon from "src/assets/img/upload-image.svg";
+import smileIcon from "src/assets/img/smile.svg";
+import { Dropdown, Upload } from "antd";
+import usePost from "src/ducks/home/post/hook";
+import Picker from "emoji-picker-react";
 
 const CommentPost: React.FC = () => {
   const { loggedUser } = useSelector(userSelector);
+  const { beforeUpload, content, setContent } = usePost();
+
+  const pickerIcon = useMemo(() => {
+    return (
+      <Picker
+        disableSearchBar={true}
+        onEmojiClick={(event, emojiObject) => {
+          setContent(content + emojiObject.emoji);
+        }}
+      />
+    );
+  }, [content, setContent]);
+
   return (
     <CommemtWrapper>
       <div className="comment-input">
@@ -31,9 +49,24 @@ const CommentPost: React.FC = () => {
                       if (e.key === "Enter") {
                         submitForm();
                       }
+                      console.log(e.key);
                     }}
                   />
                 </FormItem>
+                <div className="upload">
+                  <Upload
+                    accept={".png,.jpg,.jpeg"}
+                    name="avatar"
+                    listType="picture-card"
+                    showUploadList={false}
+                    beforeUpload={beforeUpload}
+                  >
+                    <img src={uploadIcon} alt="upload" />
+                  </Upload>
+                  <Dropdown overlay={pickerIcon} trigger={["click"]}>
+                    <img src={smileIcon} alt="" />
+                  </Dropdown>
+                </div>
               </div>
             );
           }}
--- src/components/layouts/home-feed/posts/Comments/style.ts
+++ src/components/layouts/home-feed/posts/Comments/style.ts
@@ -9,9 +9,20 @@ export const CommemtWrapper = styled.div`
       width: 100%;
       border-radius: 10px;
       background-color: #f0f2f5;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
       .ant-form-item {
         margin-bottom: 0;
         width: 70%;
+      }
+      .upload {
+        height: 70%;
+        img {
+          height: 22px;
+          margin-left: 15px;
+          cursor: pointer;
+        }
       }
     }
   }
